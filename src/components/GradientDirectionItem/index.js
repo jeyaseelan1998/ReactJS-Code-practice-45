5538ca5b66ea83defa8786750d95029dae7fd77a
jeyaseelan windows 10 <jeyaseelan.work@gmail.com>
2023-08-07 16:51:26 +0530

    code updated

--- src/components/GradientDirectionItem/index.js
+++ src/components/GradientDirectionItem/index.js
@@ -1,1 +1,22 @@
-// Write your code here
+import {Button, GradientDirectionItemContainer} from './styledComponents'
+
+const GradientDirectionItem = props => {
+  const {
+    gradientDirectionDetails,
+    isActive,
+    updateActiveGradientDirectionId,
+  } = props
+  const {directionId, displayText} = gradientDirectionDetails
+
+  const onClickHandler = () => updateActiveGradientDirectionId(directionId)
+
+  return (
+    <GradientDirectionItemContainer>
+      <Button type="button" isActive={isActive} onClick={onClickHandler}>
+        {displayText}
+      </Button>
+    </GradientDirectionItemContainer>
+  )
+}
+
+export default GradientDirectionItem
